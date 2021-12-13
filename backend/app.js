6524cdcafbf851615800153cf42ed88f46f0c501
Jim-Leroux <erenne@hotmail.fr>
2021-12-13 13:37:44 +0100

    Création du modèle, de la route et du contrôleur utilisateur

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 
+const userRoutes = require("./routes/user_routes");
+
 const app = express();
 
 mongoose
@@ -12,9 +14,21 @@ mongoose
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-app.use((req, res) => {
-  res.status(201);
-  res.json({ message: "Votre requête a bien été reçue !" });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
+
+app.use(express.json());
+
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
